f5ee9679e6629d3456b6ea8c0700e90210a5f28e
Candice Parker <lexus2424@gmail.com>
2019-06-28 22:21:25 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -71,7 +71,7 @@ def list_songs
  end
   
   def list_songs_by_genre
-   puts "Please enter the name of a genre:"
+   puts "Please enter the name of a genre:"music
    input = gets.strip
    
    if genre = Genre.find_by_name(input)
